82515d56ee4ffb47dc4363b1f6d60f846138cb8b
PM <payam.ranjbar@live.com>
2022-11-21 13:22:47 -0700

    Lockpicking with mouse

--- Assets/Scripts/HandleController.cs
+++ Assets/Scripts/HandleController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections;
-using DG.Tweening;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace DefaultNamespace
 {
@@ -10,9 +9,18 @@ namespace DefaultNamespace
         [SerializeField] private InputHandler inputHandler;
         
         [SerializeField] private Vector2 xMovementBounds;
+        [SerializeField] private Vector2 zRotationBounds;
         [SerializeField] private Transform handleTransform;
         [SerializeField] private Transform circleTransform;
         [SerializeField] private int framesToCalculateVelocity = 1;
+        [SerializeField] private float velocitySensitivity = 0.01f;
+        [SerializeField] private float positionSensitivity = 0.01f;
+
+        [Header("Sensitivity")] [SerializeField]
+        private float positionFeedbackThreshold;
+        [SerializeField] private float velocityFeedbackThreshold;
+        public UnityEvent<float> onVelocityFeedback;
+        public UnityEvent<float> onPositionFeedback;
         [SerializeField] private LockEvent @event;
         public float Velocity { get; private set; }
         private float HandleX => handleTransform.position.x;
@@ -21,12 +29,23 @@ namespace DefaultNamespace
         private void Start()
         {
             StartCoroutine(GetVelocity());
-            var initPos = (xMovementBounds.x + xMovementBounds.y) / 2;
-            handleTransform.position = new Vector3(initPos, handleTransform.position.y, handleTransform.position.z);
+            AdjustInitialPositionOfHandle();
             RotateCircle();
         }
 
-        
+        private void AdjustInitialPositionOfHandle()
+        {
+            var initPos = (xMovementBounds.x + xMovementBounds.y) / 2;
+            var position = handleTransform.position;
+            position = new Vector3(initPos, position.y, position.z);
+            handleTransform.position = position;
+        }
+
+        private void Update()
+        {
+            if(UpdateVelocity) RotateCircle();
+        }
+
         private void OnEnable()
         {
             inputHandler.onHandleClicked.AddListener(SetUpdateVelocity);
@@ -38,7 +57,7 @@ namespace DefaultNamespace
             inputHandler.onHandleClicked.RemoveListener(SetUpdateVelocity);
             inputHandler.onHandleReleased.RemoveListener(SetNotUpdateVelocity);
         }
-
+        
         private void SetUpdateVelocity() => UpdateVelocity = true;
         private void SetNotUpdateVelocity() => UpdateVelocity = false;
 
@@ -46,31 +65,68 @@ namespace DefaultNamespace
         {
             while (true)
             {
-
                 if (!UpdateVelocity)
                 {
                     yield return null;
                     continue;
                 }
-                
-                var x = HandleX;
-                var t1 = Time.time;
-                
-                for (var i = 0; i < framesToCalculateVelocity; i++)
-                {
-                    yield return null;
-                }
 
-                var t2 = Time.time;
-                var deltaX = HandleX - x;
-                var deltaT = t2 - t1;
-                Velocity = deltaX / deltaT;
-                
-                Debug.Log($"Velocity is: {Velocity}");
-                
-                RotateCircle();
+                FeedBacks();
+                yield return CheckWin();
+                yield return CalculateVelocity();
+            }
+        }
+
+        private void FeedBacks()
+        {
+            var percentage = GetPercentage();
+            
+            var positionIntensity = Mathf.Abs(@event.percentageToActive - percentage);
+            var velocityIntensity = Mathf.Abs(Mathf.Abs(Velocity) - @event.requiredVelocity);
+            
+            var positionFeedback = positionIntensity <= positionFeedbackThreshold;
+            var velocityFeedback = velocityIntensity <= velocityFeedbackThreshold;
+
+            if (velocityFeedback) onVelocityFeedback?.Invoke(velocityIntensity);
+            if (positionFeedback) onPositionFeedback?.Invoke(positionIntensity);
+        }
+        private IEnumerator CheckWin()
+        {
+            var percentage = GetPercentage();
+            var reachedPosition = Mathf.Abs(@event.percentageToActive - percentage) <= positionSensitivity;
+            var reachVelocity = Mathf.Abs(Mathf.Abs(Velocity) - @event.requiredVelocity) <= velocitySensitivity;
+
+            if (reachedPosition)
+            {
+                Debug.Log("Reach Position");
             }
 
+            if (reachVelocity)
+            {
+                Debug.Log("reached velocity");
+            }
+            if (reachedPosition && reachVelocity)
+            {
+                @event.Trigger();
+                Debug.Log("Win");
+            }
+
+            yield return null;
+        }
+        private IEnumerator CalculateVelocity()
+        {
+            var x = HandleX;
+            var t1 = Time.time;
+                
+            for (var i = 0; i < framesToCalculateVelocity; i++)
+            {
+                yield return null;
+            }
+
+            var t2 = Time.time;
+            var deltaX = HandleX - x;
+            var deltaT = t2 - t1;
+            Velocity = deltaX / deltaT;
         }
         private float GetPercentage()
         {
@@ -82,10 +138,13 @@ namespace DefaultNamespace
 
         private void RotateCircle()
         {
-            var rotation = circleTransform.rotation;
-            var minRotation = Quaternion.Euler(rotation.x, rotation.y, 0f);
-            var maxRotation = Quaternion.Euler(rotation.x, rotation.y, 360f);
-            circleTransform.rotation = Quaternion.Slerp(minRotation, maxRotation, GetPercentage());
+            var minZ = zRotationBounds.x;
+            var maxZ = zRotationBounds.y;
+            var t = GetPercentage();
+            var angle = Mathf.Lerp(minZ, maxZ, t);
+            var newRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            
+            circleTransform.rotation = newRotation;
         }
     }
 }
---
+++ Assets/Scripts/MouseHandle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class MouseHandle : InputHandler
+{
+    [SerializeField] private Vector2 xMovementBounds;
+        
+    private Vector3 _screenPoint;
+    private Vector3 _offset;
+    private Camera _camera;
+    private bool _isMouseDown;
+    private int _mouseDir;
+    private float _lastXPos;
+
+    private float MinBound => xMovementBounds.x;
+    private float MaxBound => xMovementBounds.y;
+
+    private float X => transform.position.x;
+    private bool MouseDirIsRight => _mouseDir >= 0;
+    private bool MouseDirIsLeft => _mouseDir <= 0;
+
+    private bool MouseIsStationary => _mouseDir == 0;
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        var position = transform.position;
+        _screenPoint = _camera.WorldToScreenPoint(position);
+        _offset = position - _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, _screenPoint.y, _screenPoint.z));
+        onHandleClicked?.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        onHandleReleased?.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        var curScreenPoint = new Vector3(Input.mousePosition.x, _screenPoint.y, _screenPoint.z);
+        var screenToWorld = _camera.ScreenToWorldPoint(curScreenPoint);
+        var curPosition = screenToWorld + _offset;
+            
+        var faceLeft = curPosition.x < X;
+        var faceRight = curPosition.x > X;
+            
+        if(faceLeft && ReachedLeftBound()) return;
+        if(faceRight && ReachedRightBound()) return;
+            
+        transform.position = curPosition;
+        // transform.position = Vector3.Lerp(transform.position, curPosition, Time.deltaTime);
+    }
+
+    IEnumerator CalculateMouseDir()
+    {
+        while (true)
+        {
+            _lastXPos = Input.mousePosition.x;
+            yield return null;
+            SetMouseDir();
+        }
+    }
+    private void SetMouseDir()
+    {
+        var x = Input.mousePosition.x;
+        _mouseDir = Math.Sign(x - _lastXPos);
+        _lastXPos = x;
+    }
+        
+    private bool ReachedLeftBound() => X <= MinBound;
+    private bool ReachedRightBound() => X >=  MaxBound;
+}
